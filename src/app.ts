1999e8deb3e0a411031cf2b449d85c2504b42ee6
luvihu <luvillogas@gmail.com>
2024-04-01 10:58:38 -0500

    avance

---
+++ src/app.ts
@@ -0,0 +1,16 @@
+import dotenv from 'dotenv';
+dotenv.config()
+import express from 'express';
+import authRoutes from './routes/authRoutes'
+import usersRoutes from './routes/user.Routers'
+
+const app = express()
+
+app.use(express.json())
+
+// Routes
+app.use('/auth', authRoutes)
+app.use('/users', usersRoutes)
+
+export default app
+ 
